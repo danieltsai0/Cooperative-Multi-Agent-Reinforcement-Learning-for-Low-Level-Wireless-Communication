403e70c3d3ab1b5d8f88a04291f9ca1f88036f70
colindv <colin.de.vrieze@rwth-aachen.de>
2017-04-05 18:39:18 -0700

    removed old stale modulation functions

--- functions.py
+++ functions.py
@@ -8,33 +8,6 @@
 import numpy as np
 from cmath import rect
 
-sq2 = np.sqrt(2)
-constellations = {
-    'bpsk': np.array([-1+0j, 1+0j]),
-    'qpsk': np.array([-1-1j, 1-1j, -1+1j, 1+1j]/sq2),
-    '8psk': np.array([-1-1j, -1*sq2, 1j*sq2, -1+1j, -1j*sq2, 1-1j, 1+1j, sq2]/sq2),
-    '16qam': np.array([-3+3j, -3+1j, -3-3j, -3-1j,
-                        -1+3j, -1+1j, -1-3j, -1-1j,
-                        3+3j, 3+1j, 3-3j, 3-1j,
-                        1+3j, 1+1j, 1-3j, 1-1j]/(3*sq2))
-    }
-
-def modulate():
-    """ maps a batch of bits to a given constellation
-        INPUT:
-            symbols: list or np.array of symbols to be modulated
-            constellation: list or np.array of complex-valued constellation points 
-        OUTPUT:
-            list of complex valued symbols    
-    """    
-    pass
-
-def modulate_symbol(symbol, constellation):
-    assert symbol < constellation.shape
-    return constellation[symbol]
-modulate = np.vectorize(modulate_symbol, excluded=['constellation'])
-modulate.excluded.add(1)
-
 def AWGN(samples, N0):
     """ AWGN channel
         INPUT:
