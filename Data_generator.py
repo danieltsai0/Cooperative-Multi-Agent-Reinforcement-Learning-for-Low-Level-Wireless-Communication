c29efafb1ff96ac215c5d0a72fcb790c33e98e8d
colindv <colin.de.vrieze@rwth-aachen.de>
2017-04-15 10:33:46 -0700

    generator can do pulse shaping now

--- Data_generator.py
+++ Data_generator.py
@@ -1,6 +1,3 @@
-import numpy as np
-from cmath import rect
-sq2 = np.sqrt(2)
 
 ############################################################
 #
@@ -9,7 +6,9 @@ sq2 = np.sqrt(2)
 #
 #  This script creates random binary data and modulates
 #+ the data according to a given modulation scheme with
-#+ given mapping.
+#+ given mapping. It can optionally apply pulse shaping
+#+ with given oversampling factor and root-raised cosine
+#+ filtering
 #  
 #  can be run in two ways:
 #  1. instanciate a Data_generator object and call
@@ -17,6 +16,14 @@ sq2 = np.sqrt(2)
 #  2. run from command line and load pickle file
 #
 ############################################################ 
+
+import numpy as np
+import matplotlib.pyplot as plt
+from cmath import rect
+# commpy dependency has to be installed
+from commpy.filters import rrcosfilter
+sq2 = np.sqrt(2)
+
 
 class Data_generator(object):
     """ Data generator object which can output modulated data """
@@ -80,6 +87,52 @@ class Data_generator(object):
 
         return (data_raw, data_mod)
 
+    
+    def get_pulseshaped_data(self, num, mod, os, length, alpha=0.35, Ts=1e-6, Fs=1e6):
+        """ generates random, oversampled and root-raised cosine filtered data
+            INPUT:
+                num:
+                mod:
+                os: oversampling factor
+                length: length of the rrc filter
+                alpha: roll-off factor
+                Ts: symbol period
+                Fs: sampling rate
+            OUTPUT: (data_raw, data_mod, data_shaped)
+                data_raw: integer valued np.array with raw (binary) data
+                data_mod: complex valued np.array with modulated data
+                data_os: complex valued np.array with oversampled data
+                data_shaped: complex valued np.array with pulse-shaped data
+                rrc: coefficients of the RRC filter
+        """
+        Ts = (os/2)*1e-6
+        Fs = 1e6
+        data_raw, data_mod = self.get_random_data(num, mod)        
+        rrc = rrcosfilter(length, alpha, Ts, Fs)[1]
+        os_filter = np.zeros(os)
+        os_filter[0] = 1        
+        data_os = np.kron(data_mod, os_filter)
+        # center ramp up and ramp down of filter        
+        data_os = np.concatenate([np.zeros(os/2), data_os[:-os/2]])        
+        data_shaped = np.convolve(rrc, data_os)
+        # center the filter
+        data_shaped = data_shaped[length/2:-length/2+1]       
+        return (data_raw, data_mod, data_os, data_shaped, rrc)
+
+def _main_showcase():
+    
+    # for testing the pulseshaped generator
+    generator = Data_generator()
+    data_raw, data_mod, data_os, data_shaped, rrc = generator.get_pulseshaped_data(1000, 'qpsk', 16, 128)
+    print data_raw.shape, data_mod.shape, data_os.shape, data_shaped.shape
+    plt.stem(data_os[:12*16].real, axis=0)
+    plt.plot(data_shaped[:12*16].real,'r')
+    plt.show()
+    
+    plt.scatter(data_shaped.real[:64], data_shaped.imag[:64], color='red')
+    plt.scatter(data_mod.real[:64], data_mod.imag[:64], color='blue')
+    plt.show()
+    
 
 def main():
     """ runs the data generator from the command line """
@@ -88,30 +141,62 @@ def main():
     import pickle
 
     parser = argparse.ArgumentParser()
+    
+    # normal operation    
     parser.add_argument("num", type=int, default=1000, 
                                help="number of data points to be generated (1000)")
     parser.add_argument("mod", type=str, default='qpsk', 
                                help="modulation scheme: 'bspk', 'qpsk', '8psk', '16qam' (qpsk))")
     parser.add_argument("--dir", type=str, default='./generated_data', 
                                help="directory/filename to save output to (./generated_data)")
+    
+    # for pulseshaped data    
+    parser.add_argument('--pulse', action='store_true', help="generate pulse shaped data")
+    parser.add_argument("-os", type=int, default=16, 
+                               help="oversampling factor (power of 2)")
+    parser.add_argument("-len", type=int, default=128, 
+                               help="filter length (power of 2)")
+
     args = parser.parse_args()
     num, mod, directory = (args.num, args.mod, args.dir)
+    pulse, os, length = (args.pulse, args.os, args.len)
+    
     generator = Data_generator()
 
+    # check input arguments
     try:
         pck_file = open(directory+".pck", "w")
     except IOError as error:
         print("Check if the output directory exists")
         raise
+    
+    # check if os and length are powers of 2
+    assert os&(os-1) == 0
+    assert length&(length-1) == 0
+    
+    if (pulse): # generate pulse shaped data
+    
+        data = generator.get_pulseshaped_data(num, mod, os, length)
+        # data = (data_raw, data_mod, data_os, data_shaped, rrc)
 
-    data = generator.get_random_data(num, mod)
+        # save the data 
+        pickle.dump(data, pck_file)
+        pck_file.close()
 
-    # save the data 
-    pickle.dump(data, pck_file)
-    pck_file.close()
+        print("Generated %d random samples with '%s' modulation." % (num, mod))
+        print("Signal is oversampled by a factor of %d and filtered with a rrc filter of length %d" % (os, length))
+        print("Data has been saved in %s.pck" % directory)
 
-    print("Generated %d random samples with '%s' modulation." % (num, mod))
-    print("Data has been saved in %s.pck" % directory)
+    else: # generate normal data
+        
+        data = generator.get_random_data(num, mod)
+        
+        # save the data 
+        pickle.dump(data, pck_file)
+        pck_file.close()
+
+        print("Generated %d random samples with '%s' modulation." % (num, mod))
+        print("Data has been saved in %s.pck" % directory)
     
 # in case this script gets run from command line, run the data generator and safe
 #+its output in a pickle file. run python Data_generator.py -h for help
