ba67548dd8264c91b35ffaf662dc77853fb5456d
Shane Barratt <sbarratt@berkeley.edu>
2017-04-25 14:53:17 -0700

    modular architecture ready

--- shanes_work/shane.py
+++ shanes_work/shane.py
@@ -25,11 +25,12 @@ def normc_initializer(std=1.0):
     return _initializer
 
 class NeuralTransmitter(object):
-    def __init__(self, n_bits=2, num_hidden_per_layer=[64, 32], steps_per_episode=32, stepsize=5e-3, action_dim=2, desired_kl=1e-1):
+    def __init__(self, n_bits=2, num_hidden_per_layer=[64, 32], steps_per_episode=32, stepsize=5e-3, action_dim=2, desired_kl=None, initial_logstd=-2.):
 
         self.n_hidden_layers = len(num_hidden_per_layer)
         self.num_hidden_per_layer = num_hidden_per_layer
         self.desired_kl = desired_kl
+        self.initial_logstd = initial_logstd
 
         self.n_bits = n_bits
         self.action_dim = action_dim
@@ -73,7 +74,7 @@ class NeuralTransmitter(object):
                 weights_initializer = normc_initializer(.2),
                 biases_initializer = tf.constant_initializer(0.0)
         )
-        self.x_y_logstds = -1.*tf.Variable(tf.ones(shape=self.action_dim))
+        self.x_y_logstds = self.initial_logstd*tf.Variable(tf.ones(shape=self.action_dim))
         self.x_y_logstds = tf.reshape(self.x_y_logstds, [-1, 1])
         self.x_y_logstds = tf.tile(self.x_y_logstds, [1, self.sy_batch_size])
         self.x_y_logstds = tf.transpose(self.x_y_logstds)
@@ -135,14 +136,15 @@ class NeuralTransmitter(object):
         })[0]
 
         print ('KL: %.6f' % kl)
-        if kl > self.desired_kl * 2:
-            self.stepsize /= 1.5
-            print ('stepsize -> %.6f' % self.stepsize)
-        elif kl < self.desired_kl / 2:
-            self.stepsize *= 1.5
-            print ('stepsize -> %.6f' % self.stepsize)
-        else:
-            print ('stepsize OK')
+        if self.desired_kl is not None:
+            if kl > self.desired_kl * 2:
+                self.stepsize /= 1.5
+                print ('stepsize -> %.6f' % self.stepsize)
+            elif kl < self.desired_kl / 2:
+                self.stepsize *= 1.5
+                print ('stepsize -> %.6f' % self.stepsize)
+            else:
+                print ('stepsize OK')
 
         self.reset_accum()
 
@@ -165,14 +167,14 @@ class NeuralTransmitter(object):
                     self.sy_batch_size: batch_size
             })[0]
 
-            self.xs_accum = np.r_[self.xs_accum, x_input]
-            self.actions_accum = np.r_[self.actions_accum, action]
+            self.xs_accum = np.r_[self.xs_accum, np.copy(x_input)]
+            self.actions_accum = np.r_[self.actions_accum, np.copy(action)]
             self.step += batch_size
 
         return action
 
     def receive_reward(self, rew):
-        self.adv_accum = np.r_[self.adv_accum, rew]
+        self.adv_accum = np.r_[self.adv_accum, np.copy(rew)]
 
         # If episode over, update policy and reset
         if self.step >= self.steps_per_episode:
@@ -192,11 +194,11 @@ class NeuralTransmitter(object):
             actions = self.transmit(np.array(bs)[None], evaluate=True)
             x, y = actions[0]
             plt.scatter(x, y, label=str(bs))
-            plt.annotate(str(bs), (x, y), size=size)
+            plt.annotate(''.join([str(b) for b in bs]), (x, y), size=size)
         plt.axvline(0)
         plt.axhline(0)
-        plt.xlim([-2., 2.])
-        plt.ylim([-2., 2.])
+        plt.xlim([-self.n_bits, self.n_bits])
+        plt.ylim([-self.n_bits, self.n_bits])
 
         if groundtruth:
             for k in groundtruth.keys():
@@ -205,7 +207,7 @@ class NeuralTransmitter(object):
                 if gt_labels:
                     plt.annotate(''.join([str(b) for b in k]), (x_gt, y_gt), size=size)
 
-        plt.savefig('figures/%d.png' % iteration)
+        plt.savefig('figures/%04d.png' % iteration)
         plt.close()
 
 class KNNReceiver(object):
@@ -231,6 +233,7 @@ class KNNReceiver(object):
 
 def run_simulation_distributed(n_bits, l, seed, steps_per_episode, stepsize, num_hidden_per_layer, decoding_map, sigma, desired_kl):
     env = Environment(n_bits=n_bits, l=l, sigma=sigma)
+    noise = lambda x: x + np.random.normal(loc=0.0, scale=sigma, size=(x.shape[0], 2))
     nt = NeuralTransmitter(n_bits=n_bits, steps_per_episode=steps_per_episode, stepsize=stepsize, num_hidden_per_layer=num_hidden_per_layer, desired_kl=desired_kl
             )
     knnr = KNNReceiver(n_bits=n_bits)
@@ -238,13 +241,62 @@ def run_simulation_distributed(n_bits, l, seed, steps_per_episode, stepsize, num
     for i in range(500):
         inp = np.array([env.get_input_transmitter() for _ in range(steps_per_episode)])
         tx_out = nt.transmit(inp)
+        tx_out = noise(tx_out)
         rx_out = knnr.decode(tx_out, inp)
         rew = - np.linalg.norm(rx_out - inp, ord=1, axis=1) - l * np.linalg.norm(tx_out, axis=1)**2
         nt.receive_reward(rew)
         print ("rew_mean:", np.mean(rew))
 
-        if i % 1 == 0:
+        if i % 5 == 0:
             nt.constellation(iteration=i, groundtruth=decoding_map, gt_labels=False)
+
+def run_simulation_fully_distributed(n_bits, l, seed, steps_per_episode, stepsize, num_hidden_per_layer, decoding_map, sigma, desired_kl, initial_logstd, k_innac, k_rec):
+    env = Environment(n_bits=n_bits, l=l, sigma=sigma)
+    noise = lambda x: x + np.random.normal(loc=0.0, scale=sigma, size=(x.shape[0], 2))
+    nt1 = NeuralTransmitter(n_bits=n_bits, steps_per_episode=steps_per_episode, stepsize=stepsize, num_hidden_per_layer=num_hidden_per_layer, desired_kl=desired_kl, initial_logstd=initial_logstd
+            )
+    nt2 = NeuralTransmitter(n_bits=n_bits, steps_per_episode=steps_per_episode, stepsize=stepsize, num_hidden_per_layer=num_hidden_per_layer, desired_kl=desired_kl, initial_logstd=initial_logstd
+            )
+    knnr_innac = KNNReceiver(n_bits=n_bits, k=k_innac)
+    knnr_rec = KNNReceiver(n_bits=n_bits, k=k_rec)
+
+    inp = np.array([env.get_input_transmitter() for _ in range(steps_per_episode)])
+    for i in range(500):
+        print ("\n##### Epoch %d #####" % i)
+        # 1 sends to 2
+
+        tx_out = nt1.transmit(inp)
+        tx_out = noise(tx_out)
+        rx_out = knnr_innac.decode(tx_out, inp)
+
+        tx_out1 = nt2.transmit(rx_out, evaluate=True)
+        tx_out1 = noise(tx_out1)
+        rx_out1 = knnr_rec.decode(tx_out1, inp)
+
+        rew = - np.linalg.norm(rx_out1 - inp, ord=1, axis=1) - l * np.linalg.norm(tx_out, axis=1)**2
+        nt1.receive_reward(rew)
+
+        print ("rew_mean 1:", np.mean(rew))
+
+        # 2 sends to 1
+        inp = np.array([env.get_input_transmitter() for _ in range(steps_per_episode)])
+
+        tx_out = nt2.transmit(inp)
+        tx_out = noise(tx_out)
+        rx_out = knnr_innac.decode(tx_out, inp)
+
+        tx_out1 = nt1.transmit(rx_out, evaluate=True)
+        tx_out1 = noise(tx_out1)
+        rx_out1 = knnr_rec.decode(tx_out1, inp)
+
+        rew = - np.linalg.norm(rx_out1 - inp, ord=1, axis=1) - l * np.linalg.norm(tx_out, axis=1)**2
+        nt2.receive_reward(rew)
+
+        print ("rew_mean 2:", np.mean(rew))
+
+        if i % 5 == 0:
+            nt1.constellation(iteration=i, groundtruth=decoding_map, gt_labels=False)
+
 
 # Given a decoding map, return the closest bitstring
 def rx_decode(rx_inp, decoding_map):
@@ -335,13 +387,16 @@ if __name__ == '__main__':
         (1, 1, 1, 1): 0.5/np.sqrt(2)*np.array([-3, -3])
     }
 
-    general_params = dict(stepsize=1e-3, desired_kl=2e-3, steps_per_episode=512)
+    general_params = dict(stepsize=1e-3, desired_kl=.005, initial_logstd=-1., k_innac=5, k_rec=10, l=0.05, sigma=.3)
     params = [
-        dict(seed=0, n_bits=4, decoding_map=qam16, l=.1, sigma=.2,num_hidden_per_layer=[64, 20], **general_params),
-        dict(seed=0, n_bits=2, decoding_map=psk, l=.1, sigma=.2, num_hidden_per_layer=[64, 20], **general_params),
+        dict(seed=0, n_bits=2, decoding_map=psk, num_hidden_per_layer=[32], steps_per_episode=128, **general_params),
+        dict(seed=0, n_bits=3, decoding_map=None, num_hidden_per_layer=[20], steps_per_episode=1000, **general_params),
+        dict(seed=0, n_bits=4, decoding_map=qam16, num_hidden_per_layer=[32], steps_per_episode=512, **general_params),
     ]
 
     for param in params:
-        run_simulation_distributed(**param)
+        run_simulation_fully_distributed(**param)
+        break
+        # run_simulation_distributed(**param)
         # run_simulation(**param)
 
