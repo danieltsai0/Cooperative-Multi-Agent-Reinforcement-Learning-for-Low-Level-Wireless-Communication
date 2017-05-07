08cfe3b03439d3fa6878db78d579bd7d1b8b19ca
Daniel Tsai <daniel_tsai@berkeley.edu>
2017-05-06 20:37:46 -0700

    cleaning up

--- rework/evaluate.py
+++ rework/evaluate.py
@@ -8,39 +8,8 @@ import matplotlib.pyplot as plt
 import util
 from receiver import KnnReceiver
 from channel import Channel
-from ctypes import c_char
+from ctypes import *
 
-
-#### Variables ####
-PREAMBLE_LEN = 512 # length of preamble
-TEST_LEN = 100 # length of test set
-EBN0_RANGE = (0, 16, 40) # min[dB], max[dB], #steps
-FILENAME = "output/BER_eval.csv"
-np.random.seed(0)
-n_bits = 4
-k = 3
-pp = pprint.PrettyPrinter(indent=4)
-
-cfile = ['output/centroid.pickle','output/centroid1.pickle','output/centroid2.pickle','output/centroid3.pickle',
-         'output/centroid4.pickle','output/centroid5.pickle','output/centroid6.pickle','output/centroid7.pickle',
-         'output/centroid8.pickle','output/centroid9.pickle']
-
-# #### Load centroids and generate preamble
-# parser = argparse.ArgumentParser()
-
-# parser.add_argument("cfile", type=str, nargs='*',
-#          help="path to centroids file")
-# args, leftovers = parser.parse_known_args()
-
-# Create preamble
-preamble = np.random.randint(0,2,[PREAMBLE_LEN,n_bits])
-message = np.random.randint(0,2,[TEST_LEN,n_bits])
-# Eb/N0
-ebn0_values = np.linspace(*EBN0_RANGE)
-# Other vars
-baseline_scheme = util.schemes[n_bits]
-# Mapping
-centroids = [util.schemes[n_bits]] + [pickle.load(open(cfile[i], "rb")) for i in range(len(cfile))]
 
 # Run computations
 def single_compute(centroid):
@@ -75,6 +44,39 @@ def single_compute(centroid):
     
 
 if __name__ == "__main__":
+
+    #### Variables ####
+    PREAMBLE_LEN = 512 # length of preamble
+    TEST_LEN = 100 # length of test set
+    EBN0_RANGE = (0, 16, 40) # min[dB], max[dB], #steps
+    FILENAME = "output/BER_eval.csv"
+    np.random.seed(0)
+    n_bits = 4
+    k = 3
+    pp = pprint.PrettyPrinter(indent=4)
+
+    cfile = ['output/centroid.pickle','output/centroid1.pickle','output/centroid2.pickle','output/centroid3.pickle',
+             'output/centroid4.pickle','output/centroid5.pickle','output/centroid6.pickle','output/centroid7.pickle',
+             'output/centroid8.pickle','output/centroid9.pickle']
+
+    # #### Load centroids and generate preamble
+    # parser = argparse.ArgumentParser()
+
+    # parser.add_argument("cfile", type=str, nargs='*',
+    #          help="path to centroids file")
+    # args, leftovers = parser.parse_known_args()
+
+    # Create preamble
+    preamble = np.random.randint(0,2,[PREAMBLE_LEN,n_bits])
+    message = np.random.randint(0,2,[TEST_LEN,n_bits])
+    # Eb/N0
+    ebn0_values = np.linspace(*EBN0_RANGE)
+    # Other vars
+    baseline_scheme = util.schemes[n_bits]
+    # Mapping
+    centroids = [util.schemes[n_bits]] + [pickle.load(open(cfile[i], "rb")) for i in range(len(cfile))]
+
+    
     init_string = (",".join([str(x) for x in ebn0_values]) + "\n")
     output_string = multiprocessing.Value("c_char", init_string.encode('utf-8'))
     # output_string.value = bytearray("test",'utf-8')
