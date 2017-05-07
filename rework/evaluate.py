1ef96c98cbd485edb12ee5bda1ca87aa9e5b020e
Daniel Tsai <daniel_tsai@berkeley.edu>
2017-05-06 23:05:28 -0700

    changes

--- rework/evaluate.py
+++ rework/evaluate.py
@@ -9,9 +9,14 @@ from receiver import KnnReceiver
 from channel import Channel
 from ctypes import c_char
 
+PREAMBLE_LEN = 512 # length of preamble
+TEST_LEN = 1e8 # length of test set
+    
+PREAMBLE = np.random.randint(0,2,[PREAMBLE_LEN,n_bits])
+MESSAGE = np.random.randint(0,2,[int(TEST_LEN),n_bits])
 
 # Run computations
-def single_compute(centroid, fn, ebn0_values, preamble, message, init_string):
+def single_compute(centroid, fn, ebn0_values, init_string):
     map_func = lambda x: centroid[tuple(x)]
     mean_Es = np.average([np.sum(np.square(x)) for x in list(centroid.values())])
     ber_vals = []
@@ -23,18 +28,18 @@ def single_compute(centroid, fn, ebn0_values, preamble, message, init_string):
         # Define channel
         channel = Channel(power)
         # Modulate signal
-        mod_preamble = np.array(list(map(map_func, preamble)))
-        mod_message = np.array(list(map(map_func, message)))
+        mod_preamble = np.array(list(map(map_func, PREAMBLE)))
+        mod_message = np.array(list(map(map_func, MESSAGE)))
         # Noisy signal
         mod_preamble_n = channel.AWGN(mod_preamble)
         mod_message_n = channel.AWGN(mod_message)
         # Demodulate signal
-        demod_message = util.centroid_mapping(mod_preamble_n, preamble, mod_message_n)
+        demod_message = util.centroid_mapping(mod_preamble_n, PREAMBLE, mod_message_n)
         # Compute BER
-        ber = np.sum(np.linalg.norm(demod_message - message, ord=1, axis=1)) / (TEST_LEN*n_bits)
+        ber = np.sum(np.linalg.norm(demod_message - MESSAGE, ord=1, axis=1)) / (TEST_LEN*n_bits)
         # Add to list
         ber_vals.append(str(ber))
-        # print("    EbN0: %8f, ber: %8f" % (EbN0, ber))
+        print("    EbN0: %8f, ber: %8f" % (EbN0, ber))
 
     print("    done")
 
@@ -49,9 +54,7 @@ def wrapper_func(param):
 if __name__ == "__main__":
 
     #### Variables ####
-    PREAMBLE_LEN = 512 # length of preamble
-    TEST_LEN = int(1e7) # length of test set
-    EBN0_RANGE = (0, 16, 40) # min[dB], max[dB], #steps
+    EBN0_RANGE = (0, 16, 60) # min[dB], max[dB], #steps
     FILENAME = "output/BER_eval.csv"
     base_fn = "output/%d.csv"
     np.random.seed(0)
@@ -70,8 +73,7 @@ if __name__ == "__main__":
     # args, leftovers = parser.parse_known_args()
 
     # Create preamble
-    preamble = np.random.randint(0,2,[PREAMBLE_LEN,n_bits])
-    message = np.random.randint(0,2,[TEST_LEN,n_bits])
+    
     # Eb/N0
     ebn0_values = np.linspace(*EBN0_RANGE)
     # Other vars
@@ -90,8 +92,6 @@ if __name__ == "__main__":
         run = dict(centroid=centroids[i], 
                    fn=base_fn%i, 
                    ebn0_values=ebn0_values, 
-                   preamble=preamble, 
-                   message=message, 
                    init_string=init_string)
         params.append(run)
 
