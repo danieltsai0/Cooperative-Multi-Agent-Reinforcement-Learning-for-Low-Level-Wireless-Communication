3ab509b0d3297ed98612f25e1b780140166f90dd
Daniel Tsai <daniel_tsai@berkeley.edu>
2017-05-06 21:02:22 -0700

    please work

--- rework/evaluate.py
+++ rework/evaluate.py
@@ -4,14 +4,14 @@ import numpy as np
 import multiprocessing
 import matplotlib.pyplot as plt
 
-from util import *
+import util
 from receiver import KnnReceiver
 from channel import Channel
-from ctypes import *
+from ctypes import c_char
 
 
 # Run computations
-def single_compute(centroid):
+def single_compute(centroid, fn, ebn0_values, preamble, message, init_string):
     map_func = lambda x: centroid[tuple(x)]
     mean_Es = np.average([np.sum(np.square(x)) for x in list(centroid.values())])
     ber_vals = []
@@ -29,7 +29,7 @@ def single_compute(centroid):
         mod_preamble_n = channel.AWGN(mod_preamble)
         mod_message_n = channel.AWGN(mod_message)
         # Demodulate signal
-        demod_message = centroid_mapping(mod_preamble_n, preamble, mod_message_n)
+        demod_message = util.centroid_mapping(mod_preamble_n, preamble, mod_message_n)
         # Compute BER
         ber = np.sum(np.linalg.norm(demod_message - message, ord=1, axis=1)) / (TEST_LEN*n_bits)
         # Add to list
@@ -38,15 +38,15 @@ def single_compute(centroid):
 
     print("    done")
 
-    with output_string.get_lock():
-        output_string.value += (",".join(ber_vals) + "\n").encode('utf-8')
+    with open(fn, "w") as f:
+        f.write(init_string + ",".join(ber_vals) + "\n")
     
 
 if __name__ == "__main__":
 
     #### Variables ####
     PREAMBLE_LEN = 512 # length of preamble
-    TEST_LEN = 100 # length of test set
+    TEST_LEN = int(1e7) # length of test set
     EBN0_RANGE = (0, 16, 40) # min[dB], max[dB], #steps
     FILENAME = "output/BER_eval.csv"
     np.random.seed(0)
@@ -70,15 +70,22 @@ if __name__ == "__main__":
     # Eb/N0
     ebn0_values = np.linspace(*EBN0_RANGE)
     # Other vars
-    baseline_scheme = schemes[n_bits]
+    baseline_scheme = util.schemes[n_bits]
     # Mapping
-    centroids = [baseline_scheme] + [pickle.load(open(cfile[i], "rb")) for i in range(len(cfile))]
+    centroids = [util.schemes[n_bits]] + [pickle.load(open(cfile[i], "rb")) for i in range(len(cfile))]
 
     init_string = (",".join([str(x) for x in ebn0_values]) + "\n")
-    output_string = multiprocessing.Value("c_char", init_string.encode('utf-8'))
-    # output_string.value = bytearray("test",'utf-8')
 
     print("starting ...")
+
+    base_fn = "output/%d.csv"
+    jobs = []
+    for i in range(len(centroids)):
+        p = multiprocessing.Process(target=worker, args=(centroids[i], base_fn%i, 
+                                                         ebn0_values, preamble, message,
+                                                         init_string))
+        jobs.append(p)
+        p.start()
     
     p = multiprocessing.Pool()
     p.map(single_compute, centroids)
